783392784fa451acba0e2951e662180a24908750
Orange <47792375+KChenDu@users.noreply.github.com>
2024-03-02 16:52:06 +0000

    change saving images path

--- data/demo.py
+++ data/demo.py
@@ -9,6 +9,6 @@ DATA_PATH = ROOT + '/oxford-iiit-pet'
 
 images,labels = next(iter(H5ImageLoader(DATA_PATH+'/images_train.h5', 10, DATA_PATH+'/labels_train.h5')))
 image_montage = Image.fromarray(np.concatenate([images[i] for i in range(len(images))],axis=1))
-image_montage.save(ROOT + "/train_images.jpg")
+image_montage.save(ROOT + "/images/train_images.jpg")
 label_montage = Image.fromarray(np.concatenate([labels[i] for i in range(len(labels))],axis=1))
-label_montage.save(ROOT + "/train_labels.jpg")
+label_montage.save(ROOT + "/images/train_labels.jpg")

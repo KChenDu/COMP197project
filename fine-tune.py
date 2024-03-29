fix accuracy calculation

fine-tune.py
@@ -53,14 +53,14 @@ if __name__ == '__main__':
     # losses = [torch.mean(loss).item() for loss in losses]
     # print(f'average_loss: {np.mean(losses)}')
     
-    random_img = next(iter(valid_dataloader))[0][0]
-    random_img = random_img.unsqueeze(0).to(DEVICE)
-    prediction = model(random_img)
-    print(prediction.shape)
+    # random_img = next(iter(valid_dataloader))[0][0]
+    # random_img = random_img.unsqueeze(0).to(DEVICE)
+    # prediction = model(random_img)
+    # print(prediction.shape)
     
-    # Save the image
-    org_img = transforms.ToPILImage()(random_img.squeeze(0).cpu().detach())
-    pred_img = transforms.ToPILImage()(prediction.squeeze(0).cpu().detach())
-    org_img.save('original.png')
-    pred_img.save('prediction.png')
+    # # Save the image
+    # org_img = transforms.ToPILImage()(random_img.squeeze(0).cpu().detach())
+    # pred_img = transforms.ToPILImage()(prediction.squeeze(0).cpu().detach())
+    # org_img.save('original.png')
+    # pred_img.save('prediction.png')
 

metrics.py
@@ -35,4 +35,4 @@ def accuracy(ps: Tensor, ts: Tensor) -> Tensor:
     ps = torch.round(ps).type(torch.int)
     ts = torch.round(ts).type(torch.int)
     eqs = (ps == ts).type_as(ps)
-    return sum(eqs, dim=(1, 2, 3)) / ts.numel()
+    return sum(eqs, dim=(1, 2, 3)) / ts[0].numel()